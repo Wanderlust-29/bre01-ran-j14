e145090fb7d1b697628fd07069a49d1de353578c
Wanderlust-29 <ryanroudaut8@gmail.com>
2023-11-02 09:58:15 +0000

    début exercice 5

--- exercice-4.js
+++ exercice-4.js
@@ -1,4 +1,14 @@
 window.addEventListener("DOMContentLoaded", function(){
-    // votre code ici
+    let ul = document.createElement("ul")
     let pokemons = ["Pikachu", "Salameche", "Bulbizarre", "Carapuce"];
+    document.body.appendChild(ul);
+    
+    
+    
+    for (let i = 0; i < pokemons.length; i++){
+        let pokemon = document.createTextNode(pokemons[i])
+        let li = document.createElement("li");
+        ul.appendChild(li)
+        li.appendChild(pokemon);
+    }
 });
